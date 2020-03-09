c05e2c8286e7f9624aa677a14e26ce748ba2c782
mashir0 <mashiron.0701@gmail.com>
2020-03-09 21:04:16 +0900

    1st

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,114 +1,105 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div class="top">
+    <h1>Vue学習の為のTODOアプリsample</h1>
+    <div class="kadai">
+      <ul>
+        <h3>機能</h3>
+        <li>追加</li>
+        <li>編集(double click -> enter,esc, focus out)</li>
+        <li>削除</li>
+        <h3>余力があれば</h3>
+        <li>sort</li>
+        <li>filter</li>
+        <li>double clickでfocusされないbug修正</li>
+      </ul>
+      <h3>参考サイト</h3>
+      <a href="https://jp.vuejs.org/v2/examples/todomvc.html">TodoMVC の例</a>
+    </div>
+
+    <h1>todos</h1>
+    <div class="todo">
+      <input type="text" placeholder="title" v-model="title" />
+      <button @click="addTodo" @keyup.enter="addTodo">追加</button>
+
+      <div v-if="todos.length">
+        <ul v-for="(todo, index) in todos" :key="index">
+          <li :class="{ done: todo.done }">
+            <input
+              v-if="edit == todo"
+              @keyup.enter="edit = null"
+              @keyup.esc="edit = null"
+              @blur="edit = null"
+              v-model="todo.title"
+            />
+            <div v-else>
+              <input class="toggle" type="checkbox" v-model="todo.done" />
+              <label @dblclick="edit = todo">{{ todo.title }}</label>
+              <button @click="delTodo(index)">del</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div v-else>
+        <h3>todoがありません</h3>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String
+  data() {
+    return {
+      edit: {},
+      title: "",
+      todos: []
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        title: this.title,
+        done: false,
+        edit: false
+      });
+      this.title = "";
+      this.content = "";
+    },
+
+    delTodo(index) {
+      this.todos.splice(index, 1);
+    }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.top {
+  font-size: 150%;
+  width: 80%;
+  margin: 0 auto;
+  max-width: 500px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.kadai {
+  padding: 10px;
+  border: medium solid #222222;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.kadai li {
+  display: list-item;
+  list-style: none;
 }
-a {
-  color: #42b983;
+.todo {
+}
+
+.todo li {
+  width: 80%;
+  list-style: none;
+}
+
+.done {
+  text-decoration: line-through;
 }
 </style>
