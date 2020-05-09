907a82f990184c3ffbc7060887ef4e904ce7774e
mashir0 <mashiron.0701@gmail.com>
2020-05-10 01:06:18 +0900

    横位置修正

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,47 +1,52 @@
-<template max-width="500">
+<template>
   <!-- <v-content> -->
-  <div class="pa-5 mx-auto" max-width="500">
-    <v-card class="mb-5" outlined shaped>
+  <v-container>
+    <v-card class="mx-auto mb-5" outlined shaped max-width="500">
       <v-card-title>Vuetify 学習</v-card-title>
       <v-card-text>
         TodoアプリをVuetifyで作り変え
       </v-card-text>
     </v-card>
 
-    <h1>todos</h1>
-    <v-text-field
-      outlined
-      label="title"
-      v-model="title"
-      @keyup.enter="addTodo"
-      @keyup.esc="title = ''"
-      clearable
-    >
-    </v-text-field>
+    <v-card class="mx-auto mb-5 pa-5" outlined shaped max-width="500">
+      <h1>todos</h1>
+      <v-text-field
+        outlined
+        label="title"
+        v-model="title"
+        @keyup.enter="addTodo"
+        @keyup.esc="title = ''"
+        clearable
+      >
+      </v-text-field>
 
-    <v-list v-if="todos.length">
-      <v-list-item dense v-for="(todo, index) in todos" :key="index">
-        <!-- chk box -->
-        <v-list-item-action>
-          <v-checkbox v-model="todo.done" color="primary"></v-checkbox>
-        </v-list-item-action>
+      <v-list v-if="todos.length">
+        <v-list-item dense v-for="(todo, index) in todos" :key="index">
+          <!-- chk box -->
+          <v-list-item-action>
+            <v-checkbox v-model="todo.done" color="primary"></v-checkbox>
+          </v-list-item-action>
 
-        <!-- todo -->
-        <v-list-item-content>
-          <v-list-item-title :class="{ done: todo.done }" v-text="todo.title" />
-        </v-list-item-content>
+          <!-- todo -->
+          <v-list-item-content>
+            <v-list-item-title
+              :class="{ done: todo.done }"
+              v-text="todo.title"
+            />
+          </v-list-item-content>
 
-        <!-- del btn-->
-        <v-list-item-action>
-          <v-btn outlined @click="delTodo(index)">del</v-btn>
-        </v-list-item-action>
-      </v-list-item>
-    </v-list>
+          <!-- del btn-->
+          <v-list-item-action>
+            <v-btn outlined @click="delTodo(index)">del</v-btn>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
 
-    <div v-else>
-      <h3>todoがありません</h3>
-    </div>
-  </div>
+      <div v-else>
+        <h3>todoがありません</h3>
+      </div>
+    </v-card>
+  </v-container>
   <!-- </v-content> -->
 </template>
 
