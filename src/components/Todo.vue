0c07790f804c50e99605e08c8db466cc1d0a4abb
mashir0 <mashiron.0701@gmail.com>
2020-05-11 22:50:24 +0900

    mdiフォント追加 & 詳細機能追加

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -10,34 +10,69 @@
 
     <v-card class="mx-auto mb-5 pa-5" outlined shaped max-width="500">
       <h1>todos</h1>
+      <!-- title -->
       <v-text-field
         outlined
-        label="title"
+        label="Title"
         v-model="title"
         @keyup.enter="addTodo"
         @keyup.esc="title = ''"
         clearable
-      >
-      </v-text-field>
+      />
 
+      <!-- detail -->
+      <v-textarea
+        v-if="detailFlag"
+        label="Detail"
+        auto-grow
+        rows="1"
+        row-height="15"
+        v-model="detail"
+        class="pt-0 mr-2 ml-2"
+      />
+
+      <!-- btn area -->
+      <div class="d-flex">
+        <v-btn @click="detailFlag = !detailFlag" icon class="mr-2">
+          <v-icon dark>mdi-playlist-plus</v-icon>
+        </v-btn>
+        <v-btn icon class="mr-2 mr-auto">
+          <v-icon dark>mdi-calendar-clock</v-icon>
+        </v-btn>
+
+        <v-btn v-if="detailFlag" color="primary" @click="addTodo">
+          save
+        </v-btn>
+      </div>
+
+      <!-- todo area -->
       <v-list v-if="todos.length">
-        <v-list-item dense v-for="(todo, index) in todos" :key="index">
+        <v-list-item
+          dense
+          v-for="(todo, index) in todos"
+          :key="index"
+          class="pl-0"
+        >
           <!-- chk box -->
           <v-list-item-action>
             <v-checkbox v-model="todo.done" color="primary"></v-checkbox>
           </v-list-item-action>
 
           <!-- todo -->
-          <v-list-item-content>
+          <v-list-item-content @click="showDialog($refs.dialogRef[index])">
             <v-list-item-title
               :class="{ done: todo.done }"
               v-text="todo.title"
             />
+            <!-- show dialog -->
+            <todo-dialog :todo="todo" ref="dialogRef" />
           </v-list-item-content>
 
           <!-- del btn-->
           <v-list-item-action>
-            <v-btn outlined @click="delTodo(index)">del</v-btn>
+            <v-row>
+              <v-btn outlined @click="delTodo(index)">del</v-btn>
+            </v-row>
           </v-list-item-action>
         </v-list-item>
       </v-list>
@@ -51,13 +86,21 @@
 </template>
 
 <script>
+import TodoDialog from "@/components/TodoDialog";
+
 export default {
   name: "HelloWorld",
+
+  components: {
+    TodoDialog
+  },
   data() {
     return {
       edit: {},
       title: "",
-      todos: []
+      detail: "",
+      todos: [],
+      detailFlag: false
     };
   },
   methods: {
@@ -65,16 +108,22 @@ export default {
       if (this.title) {
         this.todos.push({
           title: this.title,
+          detail: this.detail,
           done: false,
           edit: false
         });
         this.title = "";
-        this.content = "";
+        this.detail = "";
       }
     },
 
     delTodo(index) {
       this.todos.splice(index, 1);
+    },
+
+    showDialog(_refs) {
+      // console.log(_refs);
+      _refs.toggle();
     }
   }
 };
---
+++ src/components/TodoDialog.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" persistent max-width="290">
+      <!-- <template v-slot:activator="{ on }">
+        <v-btn color="primary" dark v-on="on">show</v-btn>
+      </template> -->
+      <slot v-bind:dialog="dialog" />
+
+      <v-card>
+        <v-card-title class="headline">{{ todo.title }}</v-card-title>
+        <v-card-text class="return">{{ todo.detail }}</v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="green darken-1" text @click="dialog = false"
+            >close</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "TodoDialog",
+  props: {
+    todo: Object
+  },
+
+  data() {
+    return {
+      dialog: false
+    };
+  },
+
+  methods: {
+    toggle() {
+      // console.log(this.dialog);
+      this.dialog = !this.dialog;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.return {
+  white-space: pre-wrap;
+  word-wrap: break-word;
+}
+</style>
